23875dc95da2f3a8ec5294fb80f9a46981adedbd
Yuriy Glukhov <yuriy.glukhov@gmail.com>
2015-12-09 15:47:00 +0200

    Renamed __nimx to __nimsound. Were not using nimx ;)

--- private/sound_js.nim
+++ private/sound_js.nim
@@ -14,8 +14,8 @@ proc createContext() =
         null
     );
 
-    window.__nimx_audio_context = null;
-    window.__nimx_audio_gain = null;
+    window.__nimsound_context = null;
+    window.__nimsound_gain = null;
 
     if (AudioContext) {
         var ctx = new AudioContext();
@@ -24,8 +24,8 @@ proc createContext() =
         // Connect the main volume node to the context destination.
         mainVolume.connect(ctx.destination);
 
-        window.__nimx_audio_context = ctx;
-        window.__nimx_audio_gain = mainVolume;
+        window.__nimsound_context = ctx;
+        window.__nimsound_gain = mainVolume;
     }
     else {
         console.log("Audio is not supported in your browser");
@@ -41,13 +41,13 @@ proc initWithArrayBuffer(s: Sound, ab: ref RootObj) =
     var source : ref RootObj
     var gain : ref RootObj
     {.emit: """
-    `source` = window.__nimx_audio_context.createBufferSource();
-    `gain` = window.__nimx_audio_context.createGain();
+    `source` = window.__nimsound_context.createBufferSource();
+    `gain` = window.__nimsound_context.createGain();
 
     `source`.connect(`gain`);
-    `gain`.connect(window.__nimx_audio_gain);
+    `gain`.connect(window.__nimsound_gain);
 
-    window.__nimx_audio_context.decodeAudioData(`ab`, function(buffer) {
+    window.__nimsound_context.decodeAudioData(`ab`, function(buffer) {
         `source`.buffer = buffer;
       },
 
