847d6ea9b80720e1a349ce227c7b84c9f063dcd8
Tormund <valdema_r@hotmail.com>
2016-11-17 20:02:15 +0200

    Fix get duration (#14)
    * fix leak caused by nim's realloc
    
    * fix get duration posible lock
    
    * add checks

--- sound/private/sound_sl.nim
+++ sound/private/sound_sl.nim
@@ -81,6 +81,7 @@ proc newSoundWithFile*(path: string): Sound =
     result.path = path
     result.player = nil
     result.mGain = 1
+    result.fd = -1
 
 type ResourseDescriptor {.exportc.} = object
     descriptor: int32
@@ -177,7 +178,7 @@ proc collectTrash()=
     while i < gTrash.len:
         var (item, fd, time) = gTrash[i]
         if abs(time - curTime) > TRASH_TIMEOUT:
-            {.emit: "(*`item`)->Destroy(`item`); close(`fd`);".}
+            {.emit: "(*`item`)->Destroy(`item`);if (`fd` >= 0){close(`fd`);}".}
             gTrash.delete(i)
         else:
             inc i
@@ -273,15 +274,26 @@ proc duration*(s: Sound): float =
     let pl = s.player
     if not pl.isNil:
         var msDuration : uint32 = 0xFFFFFFFFu32
+        var res:cint = 0
         {.emit: """
         SLPlayItf player;
-        int res = (*`pl`)->GetInterface(`pl`, SL_IID_PLAY, &player);
-        if (res == SL_RESULT_SUCCESS) {
-            while (`msDuration` == 0xFFFFFFFF && res == SL_RESULT_SUCCESS){
-                res = (*player)->GetDuration(player, &`msDuration`);
-            }
-        }
+        `res` = (*`pl`)->GetInterface(`pl`, SL_IID_PLAY, &player);
         """.}
+
+        if res == 0:
+            var st = 0.0
+            while msDuration == 0xFFFFFFFFu32 and res == 0:
+                {.emit: """
+                `res` = (*player)->GetDuration(player, &`msDuration`);
+                """.}
+
+                if res == 0 and msDuration == 0xFFFFFFFFu32:
+                    if st == 0.0:
+                        st = epochTime()
+                    elif epochTime() - st > 0.2:
+                        msDuration = 1000
+
+                        break
         result = float(msDuration) * 0.001
 
 #[
